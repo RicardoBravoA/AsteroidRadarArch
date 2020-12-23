af791f6f694d98aec1fc2163ab1a012afb4b034b
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-23 17:46:08 -0500

    feat: Add picture entity

---
+++ data/src/main/java/com/udacity/asteroid/data/storage/entity/PictureEntity.kt
@@ -0,0 +1,14 @@
+package com.udacity.asteroid.data.storage.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "picture")
+data class PictureEntity(
+    @PrimaryKey
+    val url: String,
+    val mediaType: String,
+    val title: String,
+    val date: String,
+    val explanation: String
+)
