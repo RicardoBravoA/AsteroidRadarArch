e59f4848adcbfe252c881df4962f58cd7f0f4d3d
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-23 18:33:34 -0500

    feat: Add asteroid data source factory

---
+++ data/src/main/java/com/udacity/asteroid/data/source/asteroid/AsteroidDataSourceFactory.kt
@@ -0,0 +1,25 @@
+package com.udacity.asteroid.data.source.asteroid
+
+import android.content.Context
+import com.udacity.asteroid.data.remote.AsteroidRemoteDataSource
+import com.udacity.asteroid.data.storage.database.AsteroidDatabase
+import com.udacity.asteroid.data.storage.source.AsteroidLocalDataSource
+
+class AsteroidDataSourceFactory(private val context: Context) {
+
+    fun create(): AsteroidDataSource {
+        val asteroidDatabase = AsteroidDatabase.getDatabase(context)
+        val value = if (context.isInternet()) Preference.CLOUD else Preference.DB
+
+        return if (Preference.CLOUD == value) {
+            AsteroidRemoteDataSource(asteroidDatabase.asteroidDao)
+        } else {
+            AsteroidLocalDataSource(asteroidDatabase.asteroidDao)
+        }
+    }
+
+    enum class Preference {
+        CLOUD, DB
+    }
+
+}
