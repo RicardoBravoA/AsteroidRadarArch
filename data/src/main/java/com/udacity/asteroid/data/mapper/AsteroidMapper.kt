438b19910aaf4a6d388f607f045c38276833f991
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-23 18:15:46 -0500

    feat: Add asteroid mapper

---
+++ data/src/main/java/com/udacity/asteroid/data/mapper/AsteroidMapper.kt
@@ -0,0 +1,66 @@
+package com.udacity.asteroid.data.mapper
+
+import com.udacity.asteroid.data.response.AsteroidResponse
+import com.udacity.asteroid.data.storage.entity.AsteroidEntity
+import com.udacity.asteroid.domain.model.AsteroidModel
+
+object AsteroidMapper {
+
+    fun transformAsteroidResponseToEntity(asteroidResponse: AsteroidResponse): AsteroidEntity {
+        return AsteroidEntity(
+            asteroidResponse.id,
+            asteroidResponse.codename,
+            asteroidResponse.closeApproachDate,
+            asteroidResponse.absoluteMagnitude,
+            asteroidResponse.estimatedDiameter,
+            asteroidResponse.relativeVelocity,
+            asteroidResponse.distanceFromEarth,
+            asteroidResponse.isPotentiallyHazardous
+        )
+    }
+
+    fun transformEntityToModel(asteroidEntityList: List<AsteroidEntity>): List<AsteroidModel> {
+        val asteroidModelList = mutableListOf<AsteroidModel>()
+
+        asteroidEntityList.forEach {
+            asteroidModelList.add(transformAsteroidEntityToModel(it))
+        }
+        return asteroidModelList
+    }
+
+    private fun transformAsteroidEntityToModel(asteroidEntity: AsteroidEntity): AsteroidModel {
+        return AsteroidModel(
+            asteroidEntity.id,
+            asteroidEntity.codename,
+            asteroidEntity.closeApproachDate,
+            asteroidEntity.absoluteMagnitude,
+            asteroidEntity.estimatedDiameter,
+            asteroidEntity.relativeVelocity,
+            asteroidEntity.distanceFromEarth,
+            asteroidEntity.isPotentiallyHazardous
+        )
+    }
+
+    fun transformResponseToModel(asteroidResponseList: List<AsteroidResponse>): List<AsteroidModel> {
+        val asteroidModelList = mutableListOf<AsteroidModel>()
+
+        asteroidResponseList.forEach {
+            asteroidModelList.add(transformAsteroidResponseToModel(it))
+        }
+        return asteroidModelList
+    }
+
+    private fun transformAsteroidResponseToModel(asteroidResponse: AsteroidResponse): AsteroidModel {
+        return AsteroidModel(
+            asteroidResponse.id,
+            asteroidResponse.codename,
+            asteroidResponse.closeApproachDate,
+            asteroidResponse.absoluteMagnitude,
+            asteroidResponse.estimatedDiameter,
+            asteroidResponse.relativeVelocity,
+            asteroidResponse.distanceFromEarth,
+            asteroidResponse.isPotentiallyHazardous
+        )
+    }
+
+}
