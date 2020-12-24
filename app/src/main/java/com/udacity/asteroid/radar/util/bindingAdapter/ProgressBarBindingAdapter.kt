1f26fc6197ec622959183ebddad8a4e3ef652bd2
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-23 23:55:53 -0500

    refactor: Remove unnused code

--- app/src/main/java/com/udacity/asteroid/radar/util/bindingAdapter/ProgressBarBindingAdapter.kt
+++ app/src/main/java/com/udacity/asteroid/radar/util/bindingAdapter/ProgressBarBindingAdapter.kt
@@ -3,6 +3,7 @@ package com.udacity.asteroid.radar.util.bindingAdapter
 import android.view.View
 import android.widget.ProgressBar
 import androidx.databinding.BindingAdapter
+import com.udacity.asteroid.radar.util.NetworkStatus
 
 @BindingAdapter("showProgressBar")
 fun ProgressBar.showProgressBar(status: NetworkStatus?) {
