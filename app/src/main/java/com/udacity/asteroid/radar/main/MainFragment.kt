df86a3ab3e9dd9bfa7f1bfa33836e1a0b6a6e471
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-23 23:58:06 -0500

    refactor: Remove unnused code

--- app/src/main/java/com/udacity/asteroid/radar/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroid/radar/main/MainFragment.kt
@@ -19,7 +19,7 @@ class MainFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
