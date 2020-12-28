e188b8fcb1d19d5b775fbdde063db02b7915ffc6
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-28 15:51:06 -0500

    feat(MainViewModelTest): Add main coroutine rule

--- app/src/test/java/com/udacity/asteroid/radar/main/MainViewModelTest.kt
+++ app/src/test/java/com/udacity/asteroid/radar/main/MainViewModelTest.kt
@@ -2,6 +2,7 @@ package com.udacity.asteroid.radar.main
 
 import com.udacity.asteroid.domain.repository.FakeAsteroidRepository
 import com.udacity.asteroid.domain.repository.FakePictureRepository
+import com.udacity.asteroid.radar.util.MainCoroutineRule
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Rule
 
@@ -11,5 +12,9 @@ class MainViewModelTest {
     private lateinit var mainViewModel: MainViewModel
     private lateinit var asteroidRepository: FakeAsteroidRepository
     private lateinit var pictureRepository: FakePictureRepository
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
 
 }
