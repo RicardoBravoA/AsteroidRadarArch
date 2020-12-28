5c39e17001fd654e2ac8c9686fe172b9f04044f5
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-28 15:47:07 -0500

    feat(MainViewModelTest): Add asteroid repository

--- app/src/test/java/com/udacity/asteroid/radar/main/MainViewModelTest.kt
+++ app/src/test/java/com/udacity/asteroid/radar/main/MainViewModelTest.kt
@@ -1,10 +1,12 @@
 package com.udacity.asteroid.radar.main
 
+import com.udacity.asteroid.domain.repository.FakeAsteroidRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @ExperimentalCoroutinesApi
 class MainViewModelTest {
 
     private lateinit var mainViewModel: MainViewModel
+    private lateinit var asteroidRepository: FakeAsteroidRepository
 
 }
