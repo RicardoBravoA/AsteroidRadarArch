14e432e552204996f6d1ce3eff8471da238f9c70
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-28 15:57:39 -0500

    feat(MainViewModelTest): Add before function

--- app/src/test/java/com/udacity/asteroid/radar/main/MainViewModelTest.kt
+++ app/src/test/java/com/udacity/asteroid/radar/main/MainViewModelTest.kt
@@ -1,10 +1,14 @@
 package com.udacity.asteroid.radar.main
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.udacity.asteroid.domain.model.AsteroidModel
+import com.udacity.asteroid.domain.model.PictureModel
 import com.udacity.asteroid.domain.repository.FakeAsteroidRepository
 import com.udacity.asteroid.domain.repository.FakePictureRepository
 import com.udacity.asteroid.radar.util.MainCoroutineRule
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
 import org.junit.Rule
 
 @ExperimentalCoroutinesApi
@@ -20,5 +24,36 @@ class MainViewModelTest {
 
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
+    private val asteroidModel1 = AsteroidModel(
+        0L, "Codename 0", "closeApproachDate 0",
+        0.0, 0.0, 0.0, 0.0, true
+    )
+
+    private val asteroidModel2 = AsteroidModel(
+        1L, "Codename 1", "closeApproachDate 1",
+        0.0, 0.0, 0.0, 0.0, true
+    )
+
+    private val asteroidModel3 = AsteroidModel(
+        2L, "Codename 2", "closeApproachDate 2",
+        0.0, 0.0, 0.0, 0.0, false
+    )
+
+    private val pictureModel = PictureModel(
+        "url", "mediaType", "title",
+        "date", "explanation"
+    )
+
+
+    @Before
+    fun setupViewModel() = mainCoroutineRule.runBlockingTest {
+        asteroidRepository = FakeAsteroidRepository()
+        pictureRepository = FakePictureRepository()
+
+        asteroidRepository.saveAsteroid(listOf(asteroidModel1, asteroidModel2, asteroidModel3))
+        pictureRepository.savePicture(pictureModel)
+
+        mainViewModel = MainViewModel(asteroidRepository, pictureRepository)
+    }
 
 }
